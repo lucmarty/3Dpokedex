8d27e1085b9ae6dbf90289860c84bfe84be9ca6b
Joris Lasserre <joris.lasserre@iut.u-bordeaux.fr>
2024-12-05 15:12:10 +0100

    link fix

--- src/components/SpritesList.tsx
+++ src/components/SpritesList.tsx
@@ -5,14 +5,15 @@ const SpriteList: React.FC = () => {
   return (
     <div id="conteneurPokemon" className="grid grid-cols-9 gap-4 justify-start overflow-x-auto p-4 bg-blue-300 rounded-xl">
       {pokedex.slice(0, 151).map((pokemon) => (
+        <a href={`/pokemon/${pokemon.name.english.toLowerCase()}`}>
         <div key={pokemon.id} className="flex flex-col items-center w-full">
           <img
             src={`/sprites/${pokemon.sprites.default}`} 
             alt={pokemon.name.french}
             className="w-16 h-16 object-contain"
           />
-          <a href={`/pokemon/${pokemon.name.english.toLowerCase()}`}><p className="text-sm text-gray-800 mt-2">{pokemon.name.french}</p> </a>
-        </div>
+          <p className="text-sm text-gray-800 mt-2">{pokemon.name.french}</p> 
+        </div></a>
       ))}
     </div>
   );
