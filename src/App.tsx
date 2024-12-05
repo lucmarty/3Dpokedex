858bfe838d0c73bd922edcca54e801d1863569fe
Joris Lasserre <joris.lasserre@iut.u-bordeaux.fr>
2024-12-05 15:18:05 +0100

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import PokemonViewer from "./components/PokemonView";
 import SpriteList from './components/SpritesList';
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Pokemon from "./components/Pokemon";
 
 const App: React.FC = () => {
   return (
-    <div className="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-6">Pokedex 3D</h1>
-      {/* <PokemonViewer modelPath="/models/eevee.glb" /> */}
-      <SpriteList />
+     <BrowserRouter>
+      <Routes>
+        {/* Route pour afficher la liste ou une page d'accueil */}
+        <Route path="/" element={<div><h1>Bienvenue dans le Pokédex</h1> <SpriteList /></div>} />
 
-    </div>
-    
+        {/* Route dynamique pour afficher les détails d'un Pokémon */}
+        <Route path="/pokemon/:pokemon" element={<Pokemon />} />
+        
+      </Routes>
+    </BrowserRouter>
     
   );
 };
