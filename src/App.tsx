ebab322a0f99e3a66759322db252298b5ae7cb7e
Johan <johan.hopital@gmail.com>
2024-12-06 14:20:52 +0100

    Add Login page

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import SpriteList from './components/SpritesList';
 import Menu from './components/Menu';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Pokemon from "./components/Pokemon";
+import Login from "./components/Login";
 
 const App: React.FC = () => {
   return (
      <BrowserRouter>
       <Routes>
-        {/* Route pour afficher la liste ou une page d'accueil */}
+        {/* Route pour afficher la page d'accueil */}
         <Route path="/" element={<><Menu /><h1>Bienvenue dans le Pokédex</h1> <SpriteList /></>} />
 
         {/* Route dynamique pour afficher les détails d'un Pokémon */}
         <Route path="/pokemon/:pokemon" element={<><Menu /> <Pokemon /></>} />
         
+        {/* Route pour afficher la page de connexion */}
+        <Route path="/login" element={<><Menu /> <Login /></>} />
       </Routes>
     </BrowserRouter>
     
