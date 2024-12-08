d281c2f19edd90b271ab2c3bad49764a164bdb0e
Johan <johan.hopital@gmail.com>
2024-12-08 12:35:58 +0100

    Add Route to Team

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import React from "react";
 import SpriteList from './components/SpritesList';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Pokemon from "./components/Pokemon";
-import Team from "./components/Team"
+import Team from "./components/Team";
+import Menu from "./components/Menu";
+import Login from "./components/Login";
 
 const App: React.FC = () => {
   return (
      <BrowserRouter>
       <Routes>
         {/* Route pour afficher la liste ou une page d'accueil */}
-        <Route path="/" element={<div><h1>Bienvenue dans le Pokédex</h1> <SpriteList /></div>} />
+        <Route path="/" element={<><Menu /><h1>Bienvenue dans le Pokédex</h1> <SpriteList /></>} />
 
         {/* Route dynamique pour afficher les détails d'un Pokémon */}
-        <Route path="/pokemon/:pokemon" element={<Pokemon />} />
+        <Route path="/pokemon/:pokemon" element={<><Menu /> <Pokemon /></>} />
 
-        <Route path="/team" element={<Team />} />     
+        <Route path="/team" element={<><Menu /> <Team /></>} />     
+        
+        <Route path="/login" element={<><Menu /> <Login /></>} />     
       </Routes>
     </BrowserRouter>    
   );
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,7 +4,7 @@ const Menu: React.FC = () => {
         <div className="bg-white p-4 shadow-lg">
             <nav className="flex justify-around">
                 <a href="/" className="text-red-500 hover:text-red-700 font-semibold"><span style={{ color: 'red' }}>3D</span><span style={{ color: 'black' }}>Pokedex</span></a>
-                <a href="/equipe" className="text-gray-500 hover:text-gray-700 font-semibold">Organisateur d'équipe</a>
+                <a href="/team" className="text-gray-500 hover:text-gray-700 font-semibold">Organisateur d'équipe</a>
                 <a href="/login" className="text-red-500 hover:text-red-700 font-semibold">Connexion</a>
             </nav>
         </div>
