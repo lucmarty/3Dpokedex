2f8dd9f509f1c81e520340227bf0f72658dfb997
Florian Chapoullié-Pino <florian.chapoullie-pino@etu.u-bordeaux.fr>
2024-12-10 14:50:51 +0100

    style(style of the pokemon page, no more overflow): no more overflow in pokemon model page

--- frontend/src/components/Pokemon.tsx
+++ frontend/src/components/Pokemon.tsx
@@ -24,9 +24,7 @@ const PokemonModel: React.FC<{ modelPath: string }> = ({ modelPath }) => {
     });
   }, [gltf]);
 
-  return (
-    <primitive object={gltf.scene} scale={0.01} position={[0, -0.5, 0]} />
-  );
+  return <primitive object={gltf.scene} scale={0.01} position={[0, -0.5, 0]} />;
 };
 
 const D3Graph: React.FC<{ stats: { [key: string]: number } }> = ({ stats }) => {
@@ -40,13 +38,24 @@ const D3Graph: React.FC<{ stats: { [key: string]: number } }> = ({ stats }) => {
     const height = 400;
     const radius = Math.min(width, height) / 2 - margin;
 
-    const statNames = ["HP", "Attack", "Defense", "Sp. Attack", "Sp. Defense", "Speed"];
+    const statNames = [
+      "HP",
+      "Attack",
+      "Defense",
+      "Sp. Attack",
+      "Sp. Defense",
+      "Speed",
+    ];
     const statValues = statNames.map((stat) => stats[stat]);
 
     const angleSlice = (Math.PI * 2) / statNames.length;
-    const rScale = d3.scaleLinear().domain([0, Math.max(...statValues)]).range([0, radius]);
+    const rScale = d3
+      .scaleLinear()
+      .domain([0, Math.max(...statValues)])
+      .range([0, radius]);
 
-    const svg = d3.select(svgRef.current)
+    const svg = d3
+      .select(svgRef.current)
       .attr("width", width)
       .attr("height", height)
       .append("g")
@@ -62,41 +71,67 @@ const D3Graph: React.FC<{ stats: { [key: string]: number } }> = ({ stats }) => {
       });
       hexagonPoints.push(hexagonPoints[0]); // Fermer l'hexagone
 
-      svg.append("path")
+      svg
+        .append("path")
         .attr("d", d3.line()(hexagonPoints)! as string)
         .style("fill", "none")
         .style("stroke", "#ddd");
     }
 
     // Dessiner les axes
-    svg.selectAll(".axis")
+    svg
+      .selectAll(".axis")
       .data(statNames)
       .enter()
       .append("line")
       .attr("class", "axis")
       .attr("x1", 0)
       .attr("y1", 0)
-      .attr("x2", (_, i) => rScale(Math.max(...statValues)) * Math.cos(angleSlice * i))
-      .attr("y2", (_, i) => rScale(Math.max(...statValues)) * Math.sin(angleSlice * i))
+      .attr(
+        "x2",
+        (_, i) => rScale(Math.max(...statValues)) * Math.cos(angleSlice * i)
+      )
+      .attr(
+        "y2",
+        (_, i) => rScale(Math.max(...statValues)) * Math.sin(angleSlice * i)
+      )
       .style("stroke", "#ccc");
 
     // Dessiner les labels des axes
-    svg.selectAll(".axis-label")
+    svg
+      .selectAll(".axis-label")
       .data(statNames)
       .enter()
       .append("text")
-      .attr("x", (_, i) => (rScale(Math.max(...statValues)) + 10) * Math.cos(angleSlice * i))
-      .attr("y", (_, i) => (rScale(Math.max(...statValues)) + 10) * Math.sin(angleSlice * i))
+      .attr(
+        "x",
+        (_, i) =>
+          (rScale(Math.max(...statValues)) + 10) * Math.cos(angleSlice * i)
+      )
+      .attr(
+        "y",
+        (_, i) =>
+          (rScale(Math.max(...statValues)) + 10) * Math.sin(angleSlice * i)
+      )
       .text((d) => d)
       .style("font-size", "10px")
       .style("text-anchor", "start");
-      
-      svg.selectAll(".axis-label")
-      .data(statValues  )
+
+    svg
+      .selectAll(".axis-label")
+      .data(statValues)
       .enter()
       .append("text")
-      .attr("x", (_, i) => (rScale(Math.max(...statValues)) + 30) * Math.cos(angleSlice * i))
-      .attr("y", (_, i) => (rScale(Math.max(...statValues)) + 30) * Math.sin(angleSlice * i))
+      .attr(
+        "x",
+        (_, i) =>
+          (rScale(Math.max(...statValues)) + 30) * Math.cos(angleSlice * i)
+      )
+      .attr(
+        "y",
+        (_, i) =>
+          (rScale(Math.max(...statValues)) + 30) * Math.sin(angleSlice * i)
+      )
       .text((d) => d)
       .style("font-size", "10px")
       .style("text-anchor", "start");
@@ -108,7 +143,8 @@ const D3Graph: React.FC<{ stats: { [key: string]: number } }> = ({ stats }) => {
     ]);
     radarPoints.push(radarPoints[0]); // Fermer le radar
 
-    svg.append("path")
+    svg
+      .append("path")
       .data([radarPoints])
       .attr("class", "radar-chart-serie")
       .attr("d", d3.line()(radarPoints)! as string)
@@ -160,9 +196,17 @@ const Pokemon: React.FC = () => {
   };
 
   return (
-    <div style={{ height: "100vh", width: "100vw", display: "flex", flexDirection: "column" }}>
+    <div
+      style={{
+        height: "calc(100vh - 56px)",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
       <div style={{ flex: 1 }}>
-        <Canvas camera={{ position: [0.5, 0.5, 2], fov: 25 }}>
+        <Canvas camera={{ position: [0, 0.5, 2], fov: 50 }}>
           <Suspense fallback={null}>
             <ambientLight intensity={1} />
             <directionalLight position={[5, 5, 5]} intensity={1} />
@@ -176,16 +220,23 @@ const Pokemon: React.FC = () => {
               minDistance={2}
               maxDistance={6}
               enablePan={false}
+              autoRotate
             />
           </Suspense>
         </Canvas>
       </div>
-      <div className="flex justify-around items-center mt-9">
-        <div>
+      <div
+        className="absolute flex items-center"
+        style={{ width: "100%" }}
+      >
+        <div className="absolute left-10">
           <D3Graph stats={stats} />
         </div>
-        <div>
-          <img src={`/sprites/${selectedPokemon.sprites.default}`} alt={selectedPokemon.name.french} />
+        <div className="absolute right-10">
+          <img
+            src={`/sprites/${selectedPokemon.sprites.default}`}
+            alt={selectedPokemon.name.french}
+          />
           <h2>{selectedPokemon.name.french}</h2>
           <p>Numéro : {selectedPokemon.id}</p>
           <p>Type :</p>
@@ -197,7 +248,9 @@ const Pokemon: React.FC = () => {
               style={{ margin: "5px" }}
             />
           ))}
-          <button onClick={handleAddPokemon}>Ajouter à la team</button>
+          <button onClick={handleAddPokemon}>
+            Ajouter à la team
+          </button>
         </div>
       </div>
     </div>
