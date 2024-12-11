7cd41c94aab79d577e0831434d752505fc6a2c01
Johan <johan.hopital@gmail.com>
2024-12-11 11:18:55 +0100

    Slider disappear if unnecessary

--- frontend/src/components/Slider.tsx
+++ frontend/src/components/Slider.tsx
@@ -11,7 +11,7 @@ const Slider: React.FC = () => {
   useEffect(() => {
     const fetchPokemon = async () => {
       try {
-        const response = await fetch(`http://localhost:5002/api/pokemons/${Math.min(151, currentId + 1)}`);
+        const response = await fetch(`http://localhost:5002/api/pokemons/${currentId + 1}`);
         if (!response.ok) {
           throw new Error("Erreur lors du chargement des données du Pokémon");
         }
@@ -28,7 +28,7 @@ const Slider: React.FC = () => {
   useEffect(() => {
     const fetchPokemon = async () => {
       try {
-        const response = await fetch(`http://localhost:5002/api/pokemons/${Math.max(1, currentId - 1)}`);
+        const response = await fetch(`http://localhost:5002/api/pokemons/${currentId - 1}`);
         if (!response.ok) {
           throw new Error("Erreur lors du chargement des données du Pokémon");
         }
@@ -51,38 +51,34 @@ const Slider: React.FC = () => {
   return (
     <div className="relative flex flex-col items-center justify-center">
       {/* Bouton gauche avec sprite */}
+      {prevPokemon && prevPokemon.id >= 1 && (
       <button
         onClick={() => handleNavigate("left")}
         className="fixed left-4 top-3/4 transform -translate-y-1/2 bg-gray-100 px-6 py-3 shadow-lg focus:outline-none ring-0 hover:ring-2 hover:ring-red-600 flex flex-col items-center"
       >
-        {prevPokemon && (
-          <>
             <img
               src={`/sprites/${prevPokemon.sprites.default}`}
               alt={prevPokemon.name.french}
               className="w-12 h-12 object-contain mb-2"
             />
             <p className="text-sm text-gray-800 mt-2">◀ {prevPokemon.name.french}</p>
-          </>
-        )}
       </button>
+      )}
 
       {/* Bouton droit avec sprite */}
+      {nextPokemon && nextPokemon.id <= 151 && (
       <button
         onClick={() => handleNavigate("right")}
         className="fixed right-4 top-3/4 transform -translate-y-1/2 bg-gray-100 px-6 py-3 shadow-lg focus:ouline-none ring-0 hover:ring-2 hover:ring-red-600 flex flex-col items-center"
       >
-        {nextPokemon && (
-          <>
             <img
               src={`/sprites/${nextPokemon.sprites.default}`}
               alt={nextPokemon.name.french}
               className="w-12 h-12 object-contain mb-2"
             />
             <p className="text-sm text-gray-800 mt-2">{nextPokemon.name.french} ▶</p>
-          </>
-        )}
         </button>
+      )}
 
     </div>
   );
