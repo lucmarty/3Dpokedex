183ca4a7a578520dd7e18dc36613d09f08c5151c
lucmarty <lucasmarty133@gmail.com>
2024-12-20 21:49:10 +0100

    add 3d models

--- src/pages/PokemonPage.tsx
+++ src/pages/PokemonPage.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useParams } from "react-router-dom";
 import { getPokemonById } from '../utils/PokemonUtils';
 import PokemonInformations from '../components/PokemonInformations';
+import Pokemon3D from '../components/Pokemon3D';
 
 const PokemonPage: React.FC = () => {    
     const { id } = useParams<{ id: string }>();
@@ -12,9 +13,18 @@ const PokemonPage: React.FC = () => {
     if (!pokemon) {
         return <div>Pokemon not found</div>;
     }
+
+    const modelPath = useMemo(() => {
+        if (!pokemon.name.english) {
+          return "";
+        }
+        return `/models/${pokemon.name.english.toLowerCase()}/${pokemon.name.english.toLowerCase()}` + ".glb";
+      }, [pokemon]);
+
     
     return (
         <div>
+            <Pokemon3D modelPath={modelPath} />
             <PokemonInformations pokemon={pokemon} />
         </div>
     );
