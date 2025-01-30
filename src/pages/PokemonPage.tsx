baa229977e4d22ea8537276af0de282b5789575a
Lucas Marty <lucasmarty133@gmail.com>
2025-01-30 10:49:22 +0100

    slider

--- src/pages/PokemonPage.tsx
+++ src/pages/PokemonPage.tsx
@@ -4,6 +4,7 @@ import { getPokemonById, getPokemonGLTFPath } from '../utils/PokemonUtils';
 import PokemonInformations from '../components/PokemonInformations';
 import Pokemon3D from '../components/Pokemon3D';
 import Statistiques from '../components/Statistiques';
+import PokemonCard from '../components/PokemonCard';
 
 const PokemonPage: React.FC = () => {
     const { id } = useParams<{ id: string }>();
@@ -16,15 +17,23 @@ const PokemonPage: React.FC = () => {
         return <div>Pokemon not found</div>;
     }
 
+    const pokemonSlider = [];
+    for (let i = -1; i < 2; i++) {
+        const nextPokemon = getPokemonById(parseInt(id) + i);
+        if (nextPokemon) {
+            pokemonSlider.push(nextPokemon);
+        }
+    }
+
     return (
         <div
             className="flex min-h-[calc(100vh-56px)] bg-cover bg-center"
             style={{
                 backgroundImage: `url('${import.meta.env.BASE_URL}backgrounds/${pokemon.type[0]}.png')`,
             }}
-        >   
-        <div className="flex flex-1 justify-center self-center"><Statistiques pokemon={pokemon.base} /></div>
-            
+        >
+            <div className="flex flex-1 justify-center self-center"><Statistiques pokemon={pokemon.base} /></div>
+
             <div className="flex flex-1">
                 {pokemon.glft ? (
                     <Pokemon3D modelPath={getPokemonGLTFPath(pokemon)} />
@@ -33,7 +42,16 @@ const PokemonPage: React.FC = () => {
                 )}
             </div>
             <div className="flex flex-1 justify-center self-center"><PokemonInformations pokemon={pokemon} /></div>
-            
+            <div className="absolute bottom-1 left-1/2 size-fit -translate-x-1/2">
+                <div className="flex justify-around gap-5 rounded-xl bg-background p-3 shadow-2xl">
+                    {pokemonSlider?.map((pokemon) => (
+                        <div className='flex flex-col items-center'>
+                            <PokemonCard key={pokemon.id} pokemon={pokemon} />
+                        </div>
+
+                    ))}
+                </div>
+            </div>
 
         </div>
     );
